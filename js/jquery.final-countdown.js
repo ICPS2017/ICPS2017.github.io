8a24337684ff56bf06900091440cb1bd07f87a00
Giulio Pasqualetti <me@giuliopasqualetti.it>
2016-09-07 14:47:19 +0200

    provo a sistemare il countdown (avanti di un mese)

--- js/jquery.final-countdown.js
+++ js/jquery.final-countdown.js
@@ -29,7 +29,7 @@
         var defaults = $.extend({
             start: '1439596800',
             end: '1502064000',
-            now: '1455293659',
+            now: '1473252372',
             selectors: {
                 value_seconds: '.clock-seconds .val',
                 canvas_seconds: 'canvas_seconds',
